f063666e224101b29a2c1db72e0d26d72cbf6f0b
codeHysteria28 <graphicglobal67@gmail.com>
2021-01-28 11:20:21 +0000

    created and styled  header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Navbar,Nav } from 'react-bootstrap';
+import style from './Header.css';
+import logo_brand from '../assets/logo_brand.png';
 
-function Header() {
+const Header = () => {
     return (
-        <div>
-            
-        </div>
+        <Navbar bg="white" expand="lg">
+            <Navbar.Brand><img src={logo_brand} alt="Logo"/></Navbar.Brand>
+            <Navbar.Toggle aria-controls="basic-navbar-nav"/>
+            <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+                <Nav>
+                    <Nav.Link href="#home">Discover</Nav.Link>
+                    <Nav.Link href="#link">Technologies</Nav.Link>
+                    <Nav.Link href="#link">Experience</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     )
 }
 
